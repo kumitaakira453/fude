1b670d4d03a7a31b71cc4d6dde082ea5ea33dce0
Kumita Akira <akira_kumita@demia.co.jp>
2026-09-14 12:46:23 +0900

    編集面の選ぶ帯に、編集面の枠を渡す

--- src/components/DocPane.tsx
+++ src/components/DocPane.tsx
@@ -1573,7 +1573,7 @@ export function DocPane({ pane, isSplit }: { pane: Pane; isSplit: boolean }) {
             span={{ from: editSel.from, to: editSel.to }}
             linkNonce={linkNonce}
             pin={scrolled}
-            within={scroller}
+            within={editScroller}
             onComment={commentOnSpan}
           />
         )}
